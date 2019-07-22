07806ed1116639d47e624193581364bdba5ecfe0
Joe <kaalm1@gmail.com>
2019-07-22 09:23:03 -0700

    second partial itieration

--- app/models/simple_spider.rb
+++ app/models/simple_spider.rb
@@ -14,7 +14,8 @@ class SimpleSpider < Kimurai::Base
   @start_urls = @csv
   @config = {
     user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
-    before_request: { delay: 4..7 }
+    disable_images: true,
+    before_request: { delay: 1..3 }
   }
 
   def parse(response, url:, data: {})
